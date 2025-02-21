e5bfb996baac352def7122b8172766f2fa62bec4
Matheus <matheusramossilva2003@gmail.com>
2025-02-21 02:05:26 -0300

    upload

---
+++ CadastroClientes/CadastroClientes/Controllers/ClienteController.cs
@@ -0,0 +1,40 @@
+﻿using CadastroClientes.Data;
+using CadastroClientes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClienteController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var clientes = _context.Clientes.ToList();
+            return View(clientes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Clientes.Add(cliente);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cliente);
+        }
+    }
+
+}
---
+++ CadastroClientes/CadastroClientes/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CadastroClientes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroClientes.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+
+}
